75d967185791857b10f90306c1d5611ee3364e36
Ankur Seth <iankurseth@gmail.com>
2023-11-21 16:16:49 +0530

    Now using mss for faster screengrabs

--- recorder.py
+++ recorder.py
@@ -1,10 +1,14 @@
-import pyautogui
+import mss
 import cv2
 import numpy as np
 
 
+
 resolution = (1920, 1080)
 fps = 30.0
+
+monitor = {"top": 0, "left": 0, "width": resolution[0], "height": resolution[1]}
+sct = mss.mss()
 
 output_filename = "recording.avi"
 
@@ -14,21 +18,30 @@ out = cv2.VideoWriter(output_filename, codec, fps, resolution)
 cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Live", 480, 270)
 
-while True:
-	img = pyautogui.screenshot()
-	frame = np.array(img)
 
-	# Convert it from BGR to RGB
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+recording_has_started = False
+recording_has_finished = False
 
-	# Write it to the output file
-	out.write(frame)
+while not recording_has_finished:
+	if recording_has_started:
+		img = sct.grab(monitor)
+		frame = np.array(img)
 
-	cv2.imshow('Live', frame)
+		# Convert it from BGR to RGB
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# Stop recording when we press 'q'
-	if cv2.waitKey(1) == ord('q'):
-		break
+		# Write it to the output file
+		out.write(frame)
+
+		cv2.imshow('Live', frame)
+
+		# Stop recording when we press 'q'
+		if cv2.waitKey(1) == ord('q'):
+			recording_has_finished = True
+
+	elif cv2.waitKey(1) == ord('s'):
+		recording_has_started = True
+	
 
 # Release the Video writer
 out.release()
