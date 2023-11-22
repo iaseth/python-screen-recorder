6ae55da82c857216e8d5411d06689189eaf59eed
Ankur Seth <iankurseth@gmail.com>
2023-11-22 10:44:52 +0530

    Now logging time_spent and frame_count, getting 25fps

--- recorder.py
+++ recorder.py
@@ -1,3 +1,5 @@
+import time
+
 import mss
 import cv2
 import numpy as np
@@ -5,7 +7,7 @@ import numpy as np
 
 
 resolution = (1920, 1080)
-fps = 30.0
+fps = 25.0
 
 monitor = {"top": 0, "left": 0, "width": resolution[0], "height": resolution[1]}
 sct = mss.mss()
@@ -21,6 +23,10 @@ cv2.resizeWindow("Live", 480, 270)
 
 recording_has_started = False
 recording_has_finished = False
+frame_count = 0
+
+start_time = 0
+end_time = 0
 
 while not recording_has_finished:
 	if recording_has_started:
@@ -32,15 +38,20 @@ while not recording_has_finished:
 
 		# Write it to the output file
 		out.write(frame)
+		frame_count += 1
 
 		cv2.imshow('Live', frame)
 
 		# Stop recording when we press 'q'
 		if cv2.waitKey(1) == ord('q'):
 			recording_has_finished = True
+			end_time = time.time()
+			print(f"Recording has finished at {round(end_time, 2)}")
 
 	elif cv2.waitKey(1) == ord('s'):
 		recording_has_started = True
+		start_time = time.time()
+		print(f"Recording has started at {round(start_time, 2)}")
 	
 
 # Release the Video writer
@@ -48,3 +59,10 @@ out.release()
  
 # Destroy all windows
 cv2.destroyAllWindows()
+
+time_spent = end_time - start_time
+fps = round(frame_count / time_spent, 2)
+
+print(f" Time Spent: {time_spent} seconds")
+print(f"Frame count: {frame_count} frames")
+print(f"    Getting: {fps} fps")
